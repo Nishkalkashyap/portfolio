2284a541369084d52d10a8baf113bdb331e30d13
Nishkal <kashyapnishkal@gmail.com>
2019-08-17 15:01:41 +0530

    removed crousel plugin

--- site/.vuepress/enhanceApp.js
+++ site/.vuepress/enhanceApp.js
@@ -1,10 +1,8 @@
-import { CarouselPlugin } from 'bootstrap-vue'
-
 export default ({
     Vue, // the version of Vue being used in the VuePress app
     // options, // the options for the root Vue instance
     router, // the router instance for the app
     // siteData // site metadata
 }) => {
-    Vue.use(CarouselPlugin);
+    
 }
