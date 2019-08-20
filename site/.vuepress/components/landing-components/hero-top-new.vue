dbaa138a395af8d197dcfa6d2d487b35d19fbd53
Nishkal <kashyapnishkal@gmail.com>
2019-08-20 13:09:28 +0530

    added service-worker event

--- site/.vuepress/components/landing-components/hero-top-new.vue
+++ site/.vuepress/components/landing-components/hero-top-new.vue
@@ -20,6 +20,7 @@
             </div>
         </div>
     </div>
+    <SWUpdatePopup :updateEvent="swUpdateEvent" />
 </div>
 </template>
 
@@ -30,6 +31,20 @@ export default {
     components: {
         SvgAnimation,
         floaters
+    },
+    data() {
+        return {
+            swUpdateEvent: null
+        };
+    },
+    mounted() {
+        this.$on("sw-updated", this.onSWUpdated);
+    },
+    methods: {
+        onSWUpdated(e) {
+            console.log(`Service Worker Updated`);
+            this.swUpdateEvent = e;
+        }
     }
 };
 </script>
@@ -133,11 +148,12 @@ export default {
 }
 
 @keyframes glow {
-  from {
-    text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #e60073, 0 0 40px #e60073, 0 0 50px #e60073, 0 0 60px #e60073, 0 0 70px #e60073;
-  }
-  to {
-    text-shadow: 0 0 20px #fff, 0 0 30px #ff4da6, 0 0 40px #ff4da6, 0 0 50px #ff4da6, 0 0 60px #ff4da6, 0 0 70px #ff4da6, 0 0 80px #ff4da6;
-  }
+    from {
+        text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #e60073, 0 0 40px #e60073, 0 0 50px #e60073, 0 0 60px #e60073, 0 0 70px #e60073;
+    }
+
+    to {
+        text-shadow: 0 0 20px #fff, 0 0 30px #ff4da6, 0 0 40px #ff4da6, 0 0 50px #ff4da6, 0 0 60px #ff4da6, 0 0 70px #ff4da6, 0 0 80px #ff4da6;
+    }
 }
 </style>
