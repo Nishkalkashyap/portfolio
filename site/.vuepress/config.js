eb00e4bcf01e86d105f66a55be6b8234d2b3edea
Nishkal <kashyapnishkal@gmail.com>
2019-08-22 16:24:14 +0530

    added hotjar tracking

--- site/.vuepress/config.js
+++ site/.vuepress/config.js
@@ -11,6 +11,18 @@ module.exports = {
     serviceWorker: true,
     head: [
         [
+            'script', {}, `
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:1454984,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        `
+        ],
+        [
             'meta', {
                 name: 'theme-color',
                 content: manifest.theme_color
