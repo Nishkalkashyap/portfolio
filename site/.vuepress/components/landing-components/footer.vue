e4b5fb97b4317d46aa992a5bfacb51a01055a2c0
Nishkal <kashyapnishkal@gmail.com>
2019-09-03 19:00:57 +0530

    added avatar

--- site/.vuepress/components/landing-components/footer.vue
+++ site/.vuepress/components/landing-components/footer.vue
@@ -1,7 +1,16 @@
 <template>
-  <div class="footer-container">
+<div class="footer-container">
+    <div class="avatar">
+        <img src="~@buildAssets/pictures/avatar.jpg" alt="avatar" align="middle">
+    </div>
+    <div class="social-icons">
+        <p class="contact">
+            <a class="block-elements" href="mailto:hello@nishkal.in?subject=Hey%20Nishkal," target="_blank">hello@nishkal.in</a>
+        </p>
+        <SocialIcons />
+    </div>
     <div class="local-container">
-      <!-- <p class="quote">
+        <!-- <p class="quote">
         Your time is limited,
         so don’t waste it living someone else’s life.
         Don’t be trapped by dogma — which is living with the results of other people’s thinking.
@@ -11,32 +20,32 @@
         Everything else is secondary.
       </p>
       <p class="title">– Steve Jobs, June 12, 2005.</p>-->
-      <p class="quote">
-        Deep in the sea
-        all molecules repeat
-        the patterns of one another
-        till complex new ones are formed.
-        They make others like themselves
-        and a new dance starts.
-        Growing in size and complexity
-        living things
-        masses of atoms
-        DNA, protein
-        dancing a pattern ever more intricate.
-        Out of the cradle
-        onto dry land
-        here it is
-        standing:
-        atoms with consciousness;
-        matter with curiosity.
-        Stands at the sea,
-        wonders at wondering: I
-        a universe of atoms
-        an atom in the Universe.
-      </p>
-      <p class="title">– Richard Feynman, Nobel Prize in Physics 1965</p>
+        <p class="quote">
+            Deep in the sea
+            all molecules repeat
+            the patterns of one another
+            till complex new ones are formed.
+            They make others like themselves
+            and a new dance starts.
+            Growing in size and complexity
+            living things
+            masses of atoms
+            DNA, protein
+            dancing a pattern ever more intricate.
+            Out of the cradle
+            onto dry land
+            here it is
+            standing:
+            atoms with consciousness;
+            matter with curiosity.
+            Stands at the sea,
+            wonders at wondering: I
+            a universe of atoms
+            an atom in the Universe.
+        </p>
+        <p class="title">– Richard Feynman, Nobel Prize in Physics 1965</p>
     </div>
-  </div>
+</div>
 </template>
 
 <script>
@@ -44,19 +53,43 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
+.avatar {
+    text-align: center;
+    margin-top: 50px;
+
+    img {
+        width: 150px;
+        height: 150px;
+        display: inline-block;
+        border-radius: 999999px;
+        object-fit: cover;
+        border: solid 4px var(--gradient-shadow);
+    }
+}
+
+.social-icons {
+    text-align: center;
+    margin-bottom: 40px;
+
+    p a {
+        color: var(--text-color);
+        border-bottom: solid 2px var(--gradient-shadow);
+    }
+}
+
 .local-container {
-  max-width: 960px;
-  margin: 0px 20px;
+    max-width: 960px;
+    margin: 0px 20px;
 
-  color: var(--text-color);
-  text-align: center;
+    color: var(--text-color);
+    text-align: center;
 
-  p.quote {
-    font-style: italic;
-  }
+    p.quote {
+        font-style: italic;
+    }
 
-  p.title {
-    font-size: 1.2em;
-  }
+    p.title {
+        font-size: 1.2em;
+    }
 }
 </style>
