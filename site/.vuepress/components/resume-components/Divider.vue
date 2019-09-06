d91cc29057cf9435ed9bac4c267847a72af8e146
Nishkal <kashyapnishkal@gmail.com>
2019-09-06 23:06:57 +0530

    added resume-components

---
+++ site/.vuepress/components/resume-components/Divider.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="divider-container">
+    <div class="divider">
+        <h3>{{title}}</h3>
+        <hr />
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    props: ['title']
+};
+</script>
+
+<style lang="scss" scoped>
+.divider {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    h3 {
+        margin: 0px !important;
+        padding-top: 0px;
+    }
+    hr {
+        overflow: auto;
+        flex-grow: 1;
+        margin-left: 20px;
+        position: relative;
+        top: 3px;
+        color: var(--divider-color);
+    }
+}
+</style>
---
+++ site/.vuepress/components/resume-components/ResumeItem.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="resume-item-container">
+    <div class="header">
+        <h4>{{title}}</h4>
+        <span>{{timeline}}</span>
+    </div>
+    <p> {{content}} </p>
+</div>
+</template>
+
+<script>
+export default {
+    props: ['title', 'timeline', 'content']
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 20px;
+}
+h4 {
+    margin: 0px !important;
+    padding: 0px;
+    font-family: var(--font-family) !important;
+}
+</style>
